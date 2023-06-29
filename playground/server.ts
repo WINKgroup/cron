941fc57e510d717444c276f6713120d72ca01799
dom <fairsayan@gmail.com>
2023-06-29 17:53:48 +0200

    consoleLog upgrade

--- playground/server.ts
+++ playground/server.ts
@@ -1,7 +1,10 @@
-import Webserver from '@winkgroup/webserver';
+import { prepareApp } from '@winkgroup/webserver';
 import CronRunnerWithWebSocket from '../src/runnerWebSocket';
 import path from 'path';
-import ConsoleLog, { LogLevel } from '@winkgroup/console-log';
+import express from 'express';
+import http from 'http';
+import { Server as IOServer } from 'socket.io';
+import ConsoleLog, { ConsoleLogLevel } from '@winkgroup/console-log';
 
 class DemoCronRunner extends CronRunnerWithWebSocket {
     async _run() {
@@ -9,18 +12,28 @@ class DemoCronRunner extends CronRunnerWithWebSocket {
     }
 }
 
-const webserver = new Webserver({ name: 'Demo Server', hasSocket: true });
-new DemoCronRunner(5, webserver.ioApp!.of('/cron-runner'), {
-    consoleLog: new ConsoleLog({
-        verbosity: LogLevel.DEBUG,
-        prefix: 'DemoCronRunner',
-    }),
-});
-webserver.app.get('/', (req: any, res: any) =>
+const app = express();
+
+prepareApp(app, 'myApp');
+app.get('/', (req: any, res: any) =>
     res.sendFile(path.join(__dirname, './controlPage.html'))
 );
 
+const server = http.createServer(app);
+const ioApp = new IOServer(server, {
+    cors: {
+        origin: true,
+    },
+});
+
+new DemoCronRunner(5, ioApp.of('/cron-runner'), {
+    consoleLog: new ConsoleLog({
+        verbosity: ConsoleLogLevel.DEBUG,
+        prefix: 'DemoCronRunner',
+    }),
+});
+
+server.listen(8080);
 console.log(
     'VISIT http://127.0.0.1:8080/ and open your browser console log to test it!'
 );
-webserver.listen();
